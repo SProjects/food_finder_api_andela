ad4b1c70bb24a694723e425a69914e21f3b51c97
Sebuuma Daniel <sedzsoft@gmail.com>
2017-04-25 11:22:58 +0300

    Adds docopt interactive which uses cmd python library.

--- food_finder.py
+++ food_finder.py
@@ -2,53 +2,94 @@
 
 Usage:
     food_finder.py find <food_name>
+    food_finder.py -i | -interactive
     food_finder.py -h | --help
     food_finder.py --version
 
 Options:
+    -i --interactive  Interactive Mode
     -h --help    show help
     --version    show version of the application
 """
 
-from docopt import docopt
+import sys, cmd
+from docopt import docopt, DocoptExit
 from app.authenticate import Api
 from app.restuarant import Restaurant
 
 
-def main(args):
-    if args['find'] and args['<food_name>']:
+def docopt_cmd(func):
+    """
+    This decorator is used to simplify the try/except block and pass the result
+    of the docopt parsing to the called action.
+    """
+    def fn(self, arg):
+        try:
+            opt = docopt(fn.__doc__, arg)
+
+        except DocoptExit as e:
+            # The DocoptExit is thrown when the args do not match.
+            # We print a message to the user and the usage block.
+            print('Invalid Command!')
+            print(e)
+            return
+
+        except SystemExit:
+            # The SystemExit exception prints the usage for --help
+            # We do not need to do the print here.
+            return
+
+        return func(self, opt)
+
+    fn.__name__ = func.__name__
+    fn.__doc__ = func.__doc__
+    fn.__dict__.update(func.__dict__)
+    return fn
+
+
+class MyInteractive(cmd.Cmd):
+    intro = 'Welcome to Food Finder!' \
+            + ' (type help for a list of commands.)'
+    prompt = '(food_finder) '
+    file = None
+
+    @docopt_cmd
+    def do_find(self, arg):
+        """Usage: find <food_name>"""
         api = Api()
-        food = args['<food_name>']
+        food = arg['<food_name>']
         query_params = {'query': food}
 
         data = api.get_data(query_params)
-        restaurants = create_restaurants(data)
-        display_top_four(restaurants, food)
-    else:
-        print 'Notice: Please run food_finder.py --help to learn the usage pattern.'
+        restaurants = self.create_restaurants(data)
+        self.display_top_four(restaurants, food)
 
+    def do_quit(self, arg):
+        """Quits out of Interactive Mode."""
+        print('Good Bye!')
+        exit()
 
-def create_restaurants(data):
-    restaurants = []
-    venues = data['response']['venues']
-    for json_venue in venues:
-        restaurants.append(Restaurant.from_json(json_venue))
-    return restaurants
+    def create_restaurants(self, data):
+        restaurants = []
+        venues = data['response']['venues']
+        for json_venue in venues:
+            restaurants.append(Restaurant.from_json(json_venue))
+        return restaurants
 
-
-def display_top_four(restaurants, food):
-    if len(restaurants) > 0:
-        print 'Top 4 venues serving {0} in Kampala'.format(food)
-        print '_______________________________'
-        print
-        for restaurant in restaurants:
-            print restaurant
-    else:
-        print 'No venues serving {0} in Kampala found.'.format(food)
-
+    def display_top_four(self, restaurants, food):
+        if len(restaurants) > 0:
+            print 'Top 4 venues serving {0} in Kampala'.format(food)
+            print '_______________________________'
+            print
+            for restaurant in restaurants:
+                print restaurant
+        else:
+            print 'No venues serving {0} in Kampala found.'.format(food)
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, argv=None, help=True, version='Food Finder v0.1', options_first=False)
-    main(arguments)
+    opt = docopt(__doc__, sys.argv[1:])
+    if opt['--interactive'] or opt['-i']:
+        MyInteractive().cmdloop()
+
 
 
